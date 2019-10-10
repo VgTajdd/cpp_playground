9451f291ca8c88bfbeafae68b7f7450a14fc0c3f
VgTajdd <aduranddiaz@gmail.com>
2019-10-10 12:19:25 -0500

    Add delete_free project

---
+++ delete_free/main.cpp
@@ -0,0 +1,17 @@
+
+// free(): is C method		(Is it necessary to use C in your C++ app?)
+// delete: is C++ operator	(faster because is a operator)
+
+int main()
+{
+	int* a = new int[5];
+	a[4] = 4;
+	int* b = a;
+
+	//free( a );	// It works but is necessary to include stdlib.h
+	//free( b );
+	//delete a;		// It works.
+	delete b;		// It works.
+
+	return 0;
+}
