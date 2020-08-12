c647a1bb3502585c7160dd7dd53df29c2bcdcd17
VgTajdd <aduranddiaz@gmail.com>
2020-08-12 16:53:20 -0500

    Update bit_fields and data_alignment projects

--- bit_fields/main.cpp
+++ bit_fields/main.cpp
@@ -23,15 +23,66 @@ struct
 	short b : 3;
 } test4;
 
-#include <stdio.h>
+
+struct bc_1
+{
+	int a : 1;
+	int b : 1;
+};
+
+struct bc_2
+{
+	int a : 31;
+	int b : 1;
+};
+
+struct bc_3
+{
+	int a : 32;
+	int b : 1;
+};
+
+struct bc_4
+{
+	int a : 31;
+	int b : 2;
+};
+
+struct bc_5
+{
+	int a : 32;
+	int b : 32;
+};
+
+/*struct bc_6
+{
+	int a : 40; // ERROR: integer only can have up to 32 bytes.
+	int b : 32;
+};
+
+struct bc_7
+{
+	int a : 63; // ERROR: integer only can have up to 32 bytes
+	int b : 1;
+};*/
+
+#include <iostream>
 
 int main()
 {
 	printf( "test1: %d\ntest2: %d\ntest3: %d\ntest4: %d\n",
-		( int )sizeof( test1 ),
-		( int )sizeof( test2 ),
-		( int )sizeof( test3 ),
-		( int )sizeof( test4 ) );
+			(int) sizeof( test1 ),
+			(int) sizeof( test2 ),
+			(int) sizeof( test3 ),
+			(int) sizeof( test4 ) );
+
+	std::cout << "1,  1:  " << sizeof( struct bc_1 ) << " bytes" << std::endl;
+	std::cout << "31, 1:  " << sizeof( struct bc_2 ) << " bytes" << std::endl;
+	std::cout << "32, 1:  " << sizeof( struct bc_3 ) << " bytes" << std::endl;
+	std::cout << "31, 2:  " << sizeof( struct bc_4 ) << " bytes" << std::endl;
+	std::cout << "32, 32: " << sizeof( struct bc_5 ) << " bytes" << std::endl;
+
+	std::cin.get();
 
 	return 0;
 }
--- data_alignment/main.cpp
+++ data_alignment/main.cpp
@@ -1,60 +1,70 @@
+/*
+
+Each object type imposes its alignment requirement on every object of that type;
+stricter alignment (with larger alignment requirement) can be requested using alignas.
+
+In order to satisfy alignment requirements of all non - static members of a class,
+padding may be inserted after some of its members.
+
+https://en.cppreference.com/w/cpp/language/object#Alignment
+
+*/
+
+/*
+Objects of type Student must be allocated at 4-byte boundaries
+because Student.id and Student.age must be allocated at 4-byte boundaries
+because int's alignment requirement is (usually) 4.
+*/
+struct Student
+{
+	char sex;	// 1 bytes. (3 padding bytes)
+	int id;		// 4 bytes.
+	char type;	// 1 byte. (3 padding bytes)
+	int age;	// 4 byte.
+}; // alignment = 4, size = 16.
+
+struct Student2
+{
+	int id;		// 4 bytes.
+	int age;	// 4 bytes.
+	char sex;	// 1 byte.
+	char type;	// 1 byte.
+}; // alignment = 4, size = 12.
+
+struct Student3
+{
+	int id;		// 4 bytes.
+	int age;	// 4 bytes.
+	char sex;	// 1 bytes.
+	char type;	// 1 bytes.
+	double x;	// 8 bytes.
+}; // alignment = 8, size = 24.
+
+struct Student4
+{
+	short id;	// 2 bytes.
+	short age;	// 2 bytes.
+	char sex;	// 1 bytes.
+	char type;	// 1 bytes.
+}; // alignment = 2, size = 6.
+
 #include <iostream>
-#include <climits>
 
-struct bc_1
+int main()
 {
-	int a : 1;
-	int b : 1;
-};
+	std::cout << "sizeof( Student ) = " << sizeof( Student ) << " bytes \t| ";
+	std::cout << "alignof( Student ) = " << alignof( Student ) << " bytes" << std::endl;
 
-struct bc_2
-{
-	int a : 31;
-	int b : 1;
-};
+	std::cout << "sizeof( Student2 ) = " << sizeof( Student2 ) << " bytes \t| ";
+	std::cout << "alignof( Student2 ) = " << alignof( Student2 ) << " bytes" << std::endl;
 
-struct bc_3
-{
-	int a : 32;
-	int b : 1;
-};
+	std::cout << "sizeof( Student3 ) = " << sizeof( Student3 ) << " bytes \t| ";
+	std::cout << "alignof( Student3 ) = " << alignof( Student3 ) << " bytes" << std::endl;
 
-struct bc_4
-{
-	int a : 31;
-	int b : 2;
-};
-
-struct bc_5
-{
-	int a : 32;
-	int b : 32;
-};
-
-/*struct bc_6
-{
-	int a : 40; // ERROR: integer only can have up to 32 bytes.
-	int b : 32;
-};
-
-struct bc_7
-{
-	int a : 63; // ERROR: integer only can have up to 32 bytes
-	int b : 1;
-};*/
-
-int main( int argc, char * argv[] )
-{
-	std::cout << "CHAR_BIT = " << CHAR_BIT;
-	std::cout << " => sizeof(int) = " << sizeof( int ) << " bytes" << std::endl;
-
-	std::cout << "1,  1:  " << sizeof( struct bc_1 ) << " bytes" << std::endl;
-	std::cout << "31, 1:  " << sizeof( struct bc_2 ) << " bytes" << std::endl;
-	std::cout << "32, 1:  " << sizeof( struct bc_3 ) << " bytes" << std::endl;
-	std::cout << "31, 2:  " << sizeof( struct bc_4 ) << " bytes" << std::endl;
-	std::cout << "32, 32: " << sizeof( struct bc_5 ) << " bytes" << std::endl;
-	//std::cout << "40, 32: " << sizeof( struct bc_6 ) << " bytes" << std::endl;
-	//std::cout << "63, 1:  " << sizeof( struct bc_7 ) << " bytes" << std::endl;
+	std::cout << "sizeof( Student4 ) = " << sizeof( Student4 ) << " bytes \t| ";
+	std::cout << "alignof( Student4 ) = " << alignof( Student4 ) << " bytes" << std::endl;
 
 	std::cin.get();
+
+	return 0;
 }
